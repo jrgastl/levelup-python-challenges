bd8e8d232b33705fe850d7452cfe42c99a003f53
Ricardo <jrgastl@gmail.com>
2025-07-03 16:55:33 +0200

    Update sort_words1.py

--- sort_words1.py
+++ sort_words1.py
@@ -1,3 +1,8 @@
+'''
+It was time to implement what I just learned in the exercise before. Also learned about the sort() method and key=str.casefold parameter.
+Again, the instructor solution was more simple and showed me how to use the join() method.
+'''
+
 import re
 
 def sort_words(phrase):
