0e7109e708db62f15ff3a956ad4d6493a1b9420b
Ricardo <jrgastl@gmail.com>
2025-07-15 13:52:15 +0000

    Starting exercise 15

--- download_sequential_files1.py
+++ download_sequential_files1.py
@@ -1,4 +1,30 @@
 '''
 Briefing:
+Write a function to download and save a sequence of files.
+Input: URL for first item, output directory path
 
+Example:
+>>> download_files('http://699340.youcanlearnit.net/image001.jpg','\images' )
+
+Successfully downloaded
+http://699340.youcanlearnit.net/image001.jpg
+
+Successfully downloaded
+http://699340.youcanlearnit.net/image002.jpg
+
+Successfully downloaded
+http://699340.youcanlearnit.net/image003.jpg
+
+.
+.
+.
+
+Successfully downloaded
+http://699340.youcanlearnit.net/image049.jpg
+
+Successfully downloaded
+http://699340.youcanlearnit.net/image050.jpg
+
+Could not retrive
+http://699340.youcanlearnit.net/image051.jpg
 '''
